9f3c160e7e034d6cc227cecb69b7c520e69a1469
gioleppe <tommasocolella95@gmail.com>
2020-09-26 14:53:35 +0200

    added automatic button recognition, added button image
    added automatic button position recognition exploiting pyautogui, removed useless set option.

--- clicko.py
+++ clicko.py
@@ -1,7 +1,5 @@
 import pyautogui as pa
 import time
-import pickle
-import sys
 import argparse
 
 
@@ -14,25 +12,12 @@ def getPosition():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="click every x seconds a certain point of the screen")
-    parser.add_argument("--set", action="store_true",
-                        help="use --set option to set the position of the button to click")
     parser.add_argument("-t", type=int, default=90, help="use this option to set sleep time between clicks")
     args = parser.parse_args()
 
-    if args.set:
-        pos = getPosition()
-        with open("conf.pkl", "wb") as f:
-            pickle.dump(pos, f)
+    pos = pa.locateCenterOnScreen('echo_save.png')
 
-    try:
-        with open("conf.pkl", "rb") as f:
-            pos = pickle.load(f)
-
-    except FileNotFoundError:
-        print("you have to set position first!, use --set option")
-        sys.exit(-1)
-
-    print("button position set to: ", pos)
+    print("button appears to be at: ", pos)
 
     clicks = 0
     secs = args.t
