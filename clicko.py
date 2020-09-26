1e3af1248e12db7449dcce9df94a901efac75a00
gioleppe <tommasocolella95@gmail.com>
2020-09-26 03:16:33 +0200

    fixed stupid error
    moved some code in order to fix business logic error

--- clicko.py
+++ clicko.py
@@ -19,18 +19,18 @@ if __name__ == '__main__':
     parser.add_argument("-t", type=int, default=90, help="use this option to set sleep time between clicks")
     args = parser.parse_args()
 
+    if args.set:
+        pos = getPosition()
+        with open("conf.pkl", "wb") as f:
+            pickle.dump(pos, f)
+
     try:
         with open("conf.pkl", "rb") as f:
             pos = pickle.load(f)
 
     except FileNotFoundError:
-        if args.set:
-            pos = getPosition()
-            with open("conf.pkl", "wb") as f:
-                pickle.dump(pos, f)
-        else:
-            print("you have to set position first!, use --set option")
-            sys.exit(-1)
+        print("you have to set position first!, use --set option")
+        sys.exit(-1)
 
     print("button position set to: ", pos)
 
@@ -38,9 +38,8 @@ if __name__ == '__main__':
     secs = args.t
     while True:
         print(f"sleeping for {secs} seconds")
-        time.sleep(secs-1)
+        time.sleep(secs - 1)
         pa.moveTo(pos, duration=1)
         pa.click(pos)
         clicks += 1
         print(f"mouse clicked at position {pos}, total clicks: {clicks}")
-
