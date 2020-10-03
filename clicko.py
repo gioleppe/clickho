dd986b5a12866ece3b9f5a38ec3292f80ad67886
gioleppe <tommasocolella95@gmail.com>
2020-10-04 00:22:48 +0200

    added quack sound

--- clicko.py
+++ clicko.py
@@ -1,11 +1,13 @@
 import pyautogui as pa
 import time
 import argparse
+from playsound import playsound
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="submits a task every x seconds (default 90)")
     parser.add_argument("-t", type=int, default=90, help="use this option to set sleep time between clicks")
+    parser.add_argument("-s", action="store_true", default=False, help="use this flag to play a sound after each submission")
     args = parser.parse_args()
 
     pos = pa.locateCenterOnScreen('echo_save.png')
@@ -21,3 +23,4 @@ if __name__ == '__main__':
         pa.click(pos)
         clicks += 1
         print(f"mouse clicked at position {pos}, total clicks: {clicks}")
+        playsound("quack.mp3")
